cef88fdaa01e77ac42af4d0ec9ea9bea09efab25
ll1zh <2901164005@qq.com>
2026-09-19 16:12:59 +0800

    Update eval.py

--- eval.py
+++ eval.py
@@ -9,26 +9,11 @@ from loss.losses import *
 from net.FPANet import FPANet
 
 eval_parser = argparse.ArgumentParser(description='Eval')
-eval_parser.add_argument('--perc', action='store_true', help='trained with perceptual loss')
 eval_parser.add_argument('--lol', action='store_true', help='output lolv1 dataset')
 eval_parser.add_argument('--lol_v2_real', action='store_true', help='output lol_v2_real dataset')
 eval_parser.add_argument('--lol_v2_syn', action='store_true', help='output lol_v2_syn dataset')
-eval_parser.add_argument('--SICE_grad', action='store_true', help='output SICE_grad dataset')
-eval_parser.add_argument('--SICE_mix', action='store_true', help='output SICE_mix dataset')
 eval_parser.add_argument('--fivek', action='store_true', help='output FiveK dataset')
 
-eval_parser.add_argument('--best_GT_mean', action='store_true', help='output lol_v2_real dataset best_GT_mean')
-eval_parser.add_argument('--best_PSNR', action='store_true', help='output lol_v2_real dataset best_PSNR')
-eval_parser.add_argument('--best_SSIM', action='store_true', help='output lol_v2_real dataset best_SSIM')
-
-eval_parser.add_argument('--custome', action='store_true', help='output custome dataset')
-eval_parser.add_argument('--custome_path', type=str, default='./YOLO')
-eval_parser.add_argument('--unpaired', action='store_true', help='output unpaired dataset')
-eval_parser.add_argument('--DICM', action='store_true', help='output DICM dataset')
-eval_parser.add_argument('--LIME', action='store_true', help='output LIME dataset')
-eval_parser.add_argument('--MEF', action='store_true', help='output MEF dataset')
-eval_parser.add_argument('--NPE', action='store_true', help='output NPE dataset')
-eval_parser.add_argument('--VV', action='store_true', help='output VV dataset')
 eval_parser.add_argument('--alpha', type=float, default=1.0)
 eval_parser.add_argument('--gamma', type=float, default=1.0)
 eval_parser.add_argument('--unpaired_weights', type=str, default='./weights/LOLv2_syn/w_perc.pth')
@@ -93,73 +78,28 @@ if __name__ == '__main__':
     if ep.lol:
         eval_data = DataLoader(dataset=get_eval_set("./datasets/LOLdataset/eval15/low"), num_workers=num_workers, batch_size=1, shuffle=False)
         output_folder = './output/LOLv1/'
-        if ep.perc:
-            weight_path = './weights/LOLv1/w_perc.pth'
-        else:
-            weight_path = './weights/MyLOLv1/LOLv1/epoch_620.pth'
+        weight_path = ''
 
     elif ep.lol_v2_real:
         eval_data = DataLoader(dataset=get_eval_set("./datasets/LOLv2/Real_captured/Test/Low"), num_workers=num_workers, batch_size=1, shuffle=False)
         output_folder = './output/LOLv2_real/'
-        if ep.best_GT_mean:
-            weight_path = './weights/LOLv2_real/w_perc.pth'
-            alpha = 0.84
-        elif ep.best_PSNR:
-            weight_path = './weights/LOLv2_real/best_PSNR.pth'
-            alpha = 0.8
-        elif ep.best_SSIM:
-            weight_path = './weights/LOLv2_real/best_SSIM.pth'
-            alpha = 0.82
+        weight_path = ''
             
     elif ep.lol_v2_syn:
         eval_data = DataLoader(dataset=get_eval_set("./datasets/LOLv2/Synthetic/Test/Low"), num_workers=num_workers, batch_size=1, shuffle=False)
         output_folder = './output/LOLv2_syn/'
-        if ep.perc:
-            weight_path = './weights/LOLv2_syn/w_perc.pth'
-        else:
-            weight_path = './weights/LOLv2_syn/wo_perc.pth'
-            
-    elif ep.SICE_grad:
-        eval_data = DataLoader(dataset=get_SICE_eval_set("./datasets/SICE/SICE_Grad"), num_workers=num_workers, batch_size=1, shuffle=False)
-        output_folder = './output/SICE_grad/'
-        weight_path = './weights/SICE.pth'
-        norm_size = False
-        
-    elif ep.SICE_mix:
-        eval_data = DataLoader(dataset=get_SICE_eval_set("./datasets/SICE/SICE_Mix"), num_workers=num_workers, batch_size=1, shuffle=False)
-        output_folder = './output/SICE_mix/'
-        weight_path = './weights/SICE.pth'
-        norm_size = False
-        
+        weight_path = ''
+
     elif ep.fivek:
         eval_data = DataLoader(dataset=get_SICE_eval_set("./datasets/FiveK/test/input"), num_workers=num_workers, batch_size=1, shuffle=False)
         output_folder = './output/fivek/'
-        weight_path = './weights/fivek.pth'
+        weight_path = ''
         norm_size = False
-    
-    elif ep.unpaired: 
-        if ep.DICM:
-            eval_data = DataLoader(dataset=get_SICE_eval_set("./datasets/DICM"), num_workers=num_workers, batch_size=1, shuffle=False)
-            output_folder = './output/DICM/'
-        elif ep.LIME:
-            eval_data = DataLoader(dataset=get_SICE_eval_set("./datasets/LIME"), num_workers=num_workers, batch_size=1, shuffle=False)
-            output_folder = './output/LIME/'
-        elif ep.MEF:
-            eval_data = DataLoader(dataset=get_SICE_eval_set("./datasets/MEF"), num_workers=num_workers, batch_size=1, shuffle=False)
-            output_folder = './output/MEF/'
-        elif ep.NPE:
-            eval_data = DataLoader(dataset=get_SICE_eval_set("./datasets/NPE"), num_workers=num_workers, batch_size=1, shuffle=False)
-            output_folder = './output/NPE/'
-        elif ep.VV:
-            eval_data = DataLoader(dataset=get_SICE_eval_set("./datasets/VV"), num_workers=num_workers, batch_size=1, shuffle=False)
-            output_folder = './output/VV/'
-        elif ep.custome:
-            eval_data = DataLoader(dataset=get_SICE_eval_set(ep.custome_path), num_workers=num_workers, batch_size=1, shuffle=False)
-            output_folder = './output/custome/'
+
         alpha = ep.alpha
         norm_size = False
         weight_path = ep.unpaired_weights
         
     eval_net = FPANet().cuda()
-    eval(eval_net, eval_data, weight_path, output_folder,norm_size=norm_size,LOL=ep.lol,v2=ep.lol_v2_real,unpaired=ep.unpaired,alpha=alpha,gamma=ep.gamma)
+    eval(eval_net, eval_data, weight_path, output_folder,norm_size=norm_size,LOL=ep.lol,v2=ep.lol_v2_real,alpha=alpha,gamma=ep.gamma)
 
